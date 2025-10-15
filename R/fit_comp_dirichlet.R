7ce464064326df8df33eacc0213d73304ebd91da
Jeffery Leirness <jeffery.leirness@noaa.gov>
2025-10-15 18:52:30 +0000

    Remove references to {dirinla} package due to installation issues

--- R/fit_comp_dirichlet.R
+++ R/fit_comp_dirichlet.R
@@ -1,52 +1,38 @@
 #' Fit a Dirichlet regression model
 #'
-#' This function fits a Dirichlet regression model using either the `dirinla::dirinlareg()` 
-#' function or the `DirichletReg::DirichReg()` function, depending on the `use_dirinla` parameter.
+#' This function fits a Dirichlet regression model using the `DirichletReg::DirichReg()`
+#' function.
 #'
-#' @param data A data frame containing the input data. Must include columns with names 
+#' @param data A data frame containing the input data. Must include columns with names
 #'   starting with "alpha_sim" for the Dirichlet parameters.
 #' @param formula A formula specifying the regression model to be fitted.
-#' @param use_dirinla Logical. If `TRUE`, uses the `dirinla::dirinlareg()` function; 
-#'   otherwise, uses `DirichletReg::DirichReg()`.
-#' @param tol0 Numeric. A tolerance parameter for `dirinla::dirinlareg()`. Ignored if `use_dirinla` is `FALSE`.
 #' @param verbose Logical. If `TRUE`, prints additional information during model fitting.
 #'
 #' @return A fitted Dirichlet regression model object.
 #' @export
-fit_comp_dirichlet <- function(data, formula, use_dirinla = FALSE, tol0 = NULL,
-                               verbose = FALSE) {
+fit_comp_dirichlet <- function(data, formula, verbose = FALSE) {
   # simulate observed substrate compositions
   alpha_sub <- data |>
     dplyr::select(dplyr::starts_with("alpha_sim")) |>
     as.matrix()
   y_sub <- DirichletReg::rdirichlet(nrow(data), alpha = alpha_sub)
-  # may need to use this data transformation if dirinla::dirinlareg() fails
+  # may need to use this data transformation.
   # note that the models ability to estimate very low substrate composition
   # probabilities may be affected by this!
   y_sub <- DirichletReg::DR_data(y_sub)
   # colnames(y_sub) <- paste0("y", 1:ncol(y_sub))
 
   # fit the model
-  if (use_dirinla) {
-    if (is.null(tol0)) {
-      dirinla::dirinlareg(formula, y = y_sub, data.cov = data, k0 = 100,
-                          prec = 0.0001, verbose = verbose)
-    } else {
-      dirinla::dirinlareg(formula, y = y_sub, data.cov = data, tol0 = tol0,
-                          k0 = 100, prec = 0.0001, verbose = verbose)
-    }
-  } else {
-    data$y <- y_sub
-    DirichletReg::DirichReg(substitute_formula, data = data) |>
-      substitute(env = list("substitute_formula" = formula)) |>
-      eval()
-  }
+  data$y <- y_sub
+  DirichletReg::DirichReg(substitute_formula, data = data) |>
+    substitute(env = list("substitute_formula" = formula)) |>
+    eval()
 }
 
 
 #' Sample data and fit a Dirichlet regression model
 #'
-#' This function samples the input data and fits a Dirichlet regression model using 
+#' This function samples the input data and fits a Dirichlet regression model using
 #' the `fit_comp_dirichlet()` function.
 #'
 #' @param data A data frame or a `PackedSpatRaster` object containing the input data.
@@ -59,22 +45,36 @@ fit_comp_dirichlet <- function(data, formula, use_dirinla = FALSE, tol0 = NULL,
 #' @param strata_var Character. The name of the variable to use for stratified sampling (optional).
 #' @param formula A formula specifying the regression model to be fitted.
 #'
-#' @return A modified version of the input `data` object, including model coefficients 
+#' @return A modified version of the input `data` object, including model coefficients
 #'   and predicted substrate composition probabilities.
 #' @export
-sample_fit_comp_dirichlet <- function(data, n, method, bias_var = NULL,
-                                      bias_thresh = NULL, clh_var = NULL,
-                                      clh_iter = NULL, strata_var = NULL,
-                                      formula) {
-
+sample_fit_comp_dirichlet <- function(
+  data,
+  n,
+  method,
+  bias_var = NULL,
+  bias_thresh = NULL,
+  clh_var = NULL,
+  clh_iter = NULL,
+  strata_var = NULL,
+  formula
+) {
   # if data is a PackedSpatRaster object, use terra::unwrap() to unpack it
-  if (inherits(data, "PackedSpatRaster")) data <- terra::unwrap(data)
+  if (inherits(data, "PackedSpatRaster")) {
+    data <- terra::unwrap(data)
+  }
 
   # sample the data
   df_sub <- data$data |>
-    rastersample::spatial_sample(n = n, method = method, bias_var = bias_var,
-                                 bias_thresh = bias_thresh, clh_var = clh_var,
-                                 clh_iter = clh_iter, strata_var = strata_var)
+    rastersample::spatial_sample(
+      n = n,
+      method = method,
+      bias_var = bias_var,
+      bias_thresh = bias_thresh,
+      clh_var = clh_var,
+      clh_iter = clh_iter,
+      strata_var = strata_var
+    )
 
   # fit dirichlet regression model
   mod <- fit_comp_dirichlet(df_sub, formula = formula)
@@ -88,26 +88,25 @@ sample_fit_comp_dirichlet <- function(data, n, method, bias_var = NULL,
 
   # return object `data`
   data
-
 }
 
 #' Predict substrate composition probabilities
 #'
-#' This function predicts substrate composition probabilities based on a fitted 
+#' This function predicts substrate composition probabilities based on a fitted
 #' Dirichlet regression model.
 #'
 #' @param data A data frame or a `PackedSpatRaster`/`SpatRaster` object containing the input data.
-#' @param model A fitted Dirichlet regression model object, either of class 
-#'   `dirinlaregmodel` (from `dirinla`) or `DirichletRegModel` (from `DirichletReg`).
+#' @param model A fitted Dirichlet regression model object of class `DirichletRegModel` (from `DirichletReg`).
 #'
-#' @return A modified version of the input `data` object, including predicted 
+#' @return A modified version of the input `data` object, including predicted
 #'   substrate composition probabilities and model-estimated alpha values.
 #'   If the input is a `SpatRaster`, the output is a wrapped raster object.
 #' @export
 predict_comp_dirichlet <- function(data, model) {
-
   # if data is a PackedSpatRaster object, use terra::unwrap() to unpack it
-  if (inherits(data, "PackedSpatRaster")) data <- terra::unwrap(data)
+  if (inherits(data, "PackedSpatRaster")) {
+    data <- terra::unwrap(data)
+  }
 
   # predict substrate composition probabilities for entire study area
   # calculate expected values from model-estimated alpha values
@@ -118,11 +117,7 @@ predict_comp_dirichlet <- function(data, model) {
   } else {
     df <- data
   }
-  if (inherits(model, "dirinlaregmodel")) {
-    n_cat <- model$ncat
-    coef_hat <- purrr::map(model$summary_fixed, .f = \(x) x$mean) |>
-      unlist()
-  } else if (inherits(model, "DirichletRegModel")) {
+  if (inherits(model, "DirichletRegModel")) {
     n_cat <- length(model$n.vars)
     coef_hat <- model$coefficients
   }
@@ -137,7 +132,7 @@ predict_comp_dirichlet <- function(data, model) {
   alpha_hat <- eta_hat |>
     exp() |>
     matrix(ncol = n_cat, byrow = TRUE)
-  p_hat <- alpha_hat / rowSums(alpha_hat)  # expected values of Dirichlet distribution with parameters equal to alpha_hat
+  p_hat <- alpha_hat / rowSums(alpha_hat) # expected values of Dirichlet distribution with parameters equal to alpha_hat
 
   if (inherits(data, "SpatRaster")) {
     r_temp <- terra::subset(data, stringr::str_c("p_sim_", 1:n_cat))
@@ -154,11 +149,12 @@ predict_comp_dirichlet <- function(data, model) {
   } else {
     data |>
       dplyr::bind_cols(tibble::as_tibble(
-        alpha_hat, .name_repair = ~ stringr::str_c("alpha_hat_", 1:n_cat))
-      ) |>
+        alpha_hat,
+        .name_repair = ~ stringr::str_c("alpha_hat_", 1:n_cat)
+      )) |>
       dplyr::bind_cols(tibble::as_tibble(
-        p_hat, .name_repair = ~ stringr::str_c("p_hat_", 1:n_cat))
-      )
+        p_hat,
+        .name_repair = ~ stringr::str_c("p_hat_", 1:n_cat)
+      ))
   }
-
 }
--- R/simulate_comp_data.R
+++ R/simulate_comp_data.R
@@ -1,19 +1,19 @@
 #' Simulate compositional data from a Dirichlet likelihood
 #'
-#' This function simulates compositional data based on a Dirichlet likelihood. It allows for 
-#' the generation of covariate rasters, polynomial transformations, and compositional probabilities 
+#' This function simulates compositional data based on a Dirichlet likelihood. It allows for
+#' the generation of covariate rasters, polynomial transformations, and compositional probabilities
 #' for a specified number of components. The output can be returned as a raster object or a data frame.
 #'
-#' @param x A `SpatRaster` or `PackedSpatRaster` object containing covariate data. If it is a 
+#' @param x A `SpatRaster` or `PackedSpatRaster` object containing covariate data. If it is a
 #'   `PackedSpatRaster`, it will be unpacked using `terra::unwrap()`.
 #' @param d Integer. The number of compositional components to simulate.
-#' @param layers A character vector or numeric vector specifying the layers of `x` to use as covariates. 
+#' @param layers A character vector or numeric vector specifying the layers of `x` to use as covariates.
 #'   If `NULL`, all layers are used or a subset is selected based on `n_cov_sim`.
-#' @param poly_degree Integer. The degree of polynomial transformations to apply to the covariates. 
+#' @param poly_degree Integer. The degree of polynomial transformations to apply to the covariates.
 #'   If greater than 1, additional layers are generated as powers of the original covariates.
-#' @param n_cov_sim Integer. The number of covariate layers to randomly select for simulation. 
+#' @param n_cov_sim Integer. The number of covariate layers to randomly select for simulation.
 #'   Ignored if `layers` is specified.
-#' @param as_raster Logical. If `TRUE`, the output includes simulated data as a `SpatRaster` object. 
+#' @param as_raster Logical. If `TRUE`, the output includes simulated data as a `SpatRaster` object.
 #'   If `FALSE`, the output is a data frame.
 #' @param seed Integer. A random seed for reproducibility. If `NULL`, no seed is set.
 #'
@@ -26,7 +26,7 @@
 #'   - `seed`: The random seed used for simulation.
 #'   - `form_sim`: The formula used to generate the simulated data.
 #'   - `coef_sim`: The coefficients used in the simulation.
-#'   - `data`: A `SpatRaster` object (if `as_raster = TRUE`) or a data frame containing the simulated 
+#'   - `data`: A `SpatRaster` object (if `as_raster = TRUE`) or a data frame containing the simulated
 #'     compositional probabilities (`p_sim_*`) and Dirichlet parameters (`alpha_sim_*`).
 #'
 #' @details The function performs the following steps:
@@ -44,21 +44,32 @@
 #' sim_data <- simulate_comp_data(x = covariates, d = 3, poly_degree = 2, as_raster = TRUE)
 #'
 #' @export
-simulate_comp_data <- function(x, d, layers = NULL, poly_degree = NULL,
-                               n_cov_sim = NULL, as_raster = FALSE,
-                               seed = NULL) {
-
+simulate_comp_data <- function(
+  x,
+  d,
+  layers = NULL,
+  poly_degree = NULL,
+  n_cov_sim = NULL,
+  as_raster = FALSE,
+  seed = NULL
+) {
   # set random number generator seed
-  if (is.numeric(seed)) set.seed(seed)
+  if (is.numeric(seed)) {
+    set.seed(seed)
+  }
 
   # if x is a PackedSpatRaster object, use terra::unwrap() to unpack it
-  if (inherits(x, "PackedSpatRaster")) x <- terra::unwrap(x)
+  if (inherits(x, "PackedSpatRaster")) {
+    x <- terra::unwrap(x)
+  }
 
   # create covariate polynomial rasters
   if (is.null(layers) & !is.null(poly_degree)) {
     if (poly_degree > 1) {
-      x_add <- purrr::map(2:poly_degree, \(deg) x^deg |>
-                            stats::setNames(nm = stringr::str_c(names(x), "^", deg))) |>
+      x_add <- purrr::map(2:poly_degree, \(deg) {
+        x^deg |>
+          stats::setNames(nm = stringr::str_c(names(x), "^", deg))
+      }) |>
         terra::rast()
       x <- c(x, x_add)
     }
@@ -70,64 +81,41 @@ simulate_comp_data <- function(x, d, layers = NULL, poly_degree = NULL,
     }
   }
 
+  # convert SpatRaster to data frame
+  df <- x |>
+    terra::as.data.frame(cells = TRUE, na.rm = TRUE) |>
+    tibble::as_tibble()
+
   # specify model formula structure that generates the simulated data
   if (is.character(layers)) {
     layer_names <- layers
   } else if (is.numeric(layers)) {
     layer_names <- names(x)[layers]
   }
-  form_sim <- layer_names |>
-    stringr::str_flatten(collapse = " + ") |>
-    pipebind::bind(._, stringr::str_c("1 + ", ._)) |>
-    rep.int(times = d) |>
+  form_base <- stringr::str_flatten(layer_names, collapse = " + ") |>
+    pipebind::bind(._, stringr::str_c("1 + ", ._))
+  form_components <- rep(list(form_base), d)
+  form_sim <- form_components |>
     stringr::str_flatten(collapse = " | ") |>
     pipebind::bind(._, stringr::str_c("y ~ ", ._)) |>
     stats::as.formula()
 
-
   # simulate compositional data ---------------------------------------------
-
-  # # method 1: create SpatRaster object containing simulated substrate composition
-  # # probabilities
-  # # linear effects formulation
-  # x_sim <- stats::rnorm(d * 2, mean = 0, sd = 2) |>
-  #   matrix(nrow = d) |>
-  #   tibble::as_tibble(.name_repair = ~ paste0("beta_", 0:1))
-  #
-  # # create simulated substrate composition probabilities as SpatRaster objects
-  # r_sim <- terra::subset(x, subset = layers) * x_sim$beta_1 + x_sim$beta_0
-  # alpha_sim <- exp(r_sim)
-  # names(alpha_sim) <- paste0("alpha_sim", 1:terra::nlyr(alpha_sim))
-  # r_sim <- alpha_sim / sum(alpha_sim)
-  # names(r_sim) <- paste0("p_sim", 1:terra::nlyr(r_sim))
-  #
-  # # create SpatRaster object containing substrate composition probabilities and covariates
-  # r <- list(r_sim, alpha_sim, terra::subset(x, subset = layers)) |>
-  #   terra::rast()
-  #
-  # # create data frame (tibble) containing substrate composition probabilities and covariates
-  # df <- rastersample_prep(r, cells = TRUE)
-
-  # method 2: create dgCMatrix object via data_stack_dirich() function
-  # this makes it easier to automate the matrix algebra based on the structure of
-  # form_sim
-  df <- x |>
-    terra::as.data.frame(cells = TRUE, na.rm = TRUE) |>
-    tibble::as_tibble()
-  ds_sim <- dirinla::data_stack_dirich(
-    y = rep.int(NA, times = nrow(df) * d),
-    covariates = dirinla::formula_list(form_sim),
-    data = df,
-    d = d,
-    n = nrow(df)
-  )
-  x_sim <- ncol(ds_sim) |>
-    stats::rnorm(mean = 0, sd = 2)
-  eta_sim <- ds_sim %*% x_sim
-  alpha_sim <- eta_sim |>
-    exp() |>
-    matrix(ncol = d, byrow = TRUE)
-  p_sim <- alpha_sim / rowSums(alpha_sim)  # expected values of Dirichlet distribution with parameters equal to alpha
+  x_sim <- list()
+  eta_sim <- matrix(NA, nrow = nrow(df), ncol = d)
+  for (i in 1:d) {
+    vars <- stringr::str_remove(form_components[[i]], pattern = "^1 \\+ ") |>
+      stringr::str_split(" \\+ ") |>
+      purrr::pluck(1)
+    design_matrix <- model.matrix(
+      ~.,
+      data = dplyr::select(df, dplyr::all_of(vars))
+    )
+    x_sim[[i]] <- stats::rnorm(ncol(design_matrix), mean = 0, sd = 2)
+    eta_sim[, i] <- design_matrix %*% x_sim[[i]]
+  }
+  alpha_sim <- exp(eta_sim)
+  p_sim <- alpha_sim / rowSums(alpha_sim) # expected values of Dirichlet distribution with parameters equal to alpha
 
   if (inherits(x, "SpatRaster") & as_raster) {
     r_temp <- terra::subset(x, subset = 1)
@@ -141,10 +129,17 @@ simulate_comp_data <- function(x, d, layers = NULL, poly_degree = NULL,
     x <- c(x, r_alpha_sim)
     names(r_p_sim) <- stringr::str_c("p_sim_", 1:d)
     x <- c(x, r_p_sim)
-    tibble::lst(d = d, layers = layers, poly_degree = poly_degree,
-                n_cov_sim = n_cov_sim, as_raster = as_raster, seed = seed,
-                form_sim = form_sim, coef_sim = x_sim,
-                data = terra::wrap(x))
+    tibble::lst(
+      d = d,
+      layers = layers,
+      poly_degree = poly_degree,
+      n_cov_sim = n_cov_sim,
+      as_raster = as_raster,
+      seed = seed,
+      form_sim = form_sim,
+      coef_sim = x_sim,
+      data = terra::wrap(x)
+    )
   } else {
     df <- df |>
       dplyr::bind_cols(
@@ -153,8 +148,16 @@ simulate_comp_data <- function(x, d, layers = NULL, poly_degree = NULL,
       dplyr::bind_cols(
         tibble::as_tibble(p_sim, .name_repair = ~ paste0("p_sim_", 1:d))
       )
-    tibble::lst(d = d, layers = layers, poly_degree = poly_degree,
-                n_cov_sim = n_cov_sim, as_raster = as_raster, seed = seed,
-                form_sim = form_sim, coef_sim = x_sim, data = df)
+    tibble::lst(
+      d = d,
+      layers = layers,
+      poly_degree = poly_degree,
+      n_cov_sim = n_cov_sim,
+      as_raster = as_raster,
+      seed = seed,
+      form_sim = form_sim,
+      coef_sim = x_sim,
+      data = df
+    )
   }
 }
